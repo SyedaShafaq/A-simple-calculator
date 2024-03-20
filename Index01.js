012866e0b3853843ad8612a3090ffb914bee50cc
SyedaShafaq <shafaqfatima161@gmail.com>
2024-03-20 21:05:21 +0500

    Push my files

---
+++ Index01.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter First Number",
+        type: "number",
+        name: "operator1",
+    },
+    {
+        message: "Enter Second Number",
+        type: "number",
+        name: "operator2",
+    },
+    {
+        message: "Please select operator to perform operation",
+        type: "list",
+        name: "operator3",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division",
+            "Module",
+        ],
+    },
+]);
+console.log(answer);
+if (answer.operator3 === "Addition") {
+    console.log("Result");
+    console.log(answer.operator1 + answer.operator2);
+}
+else if (answer.operator3 === "Subtraction") {
+    console.log("Result");
+    console.log(answer.operator1 - answer.operator2);
+}
+else if (answer.operator3 === "Multiplication") {
+    console.log("Result");
+    console.log(answer.operator1 * answer.operator2);
+}
+else if (answer.operator3 === "Division") {
+    console.log("Result");
+    console.log(answer.operator1 / answer.operator2);
+}
+else if (answer.operator3 === "Module") {
+    console.log("Result");
+    console.log(answer.operator1 % answer.operator2);
+}
+else {
+    console.log("Please choose valid operator");
+}
